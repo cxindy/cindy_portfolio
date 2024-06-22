cd375d1b4efeeb8cb20d667524d2e12f27d9cb26
Cindy Swinfield <cswinfield@deloitte.com.au>
2024-06-23 00:58:45 +1000

    Adding light and dark mode for the cat

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,11 @@
 const lightIcon = document.getElementById("light-icon");
 const darkIcon = document.getElementById("dark-icon");
 
+const catSun = document.getElementById("sun");
+const catCloud = document.getElementById("cloud");
+const catMoon = document.getElementById("moon");
+const catStars = document.getElementById("star");
+
 // Check if dark mode is preferred
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: light)");
 let darkMode = darkModeMediaQuery.matches;
@@ -9,8 +14,12 @@ let darkMode = darkModeMediaQuery.matches;
 if (darkMode) {
   document.body.classList.add("dark-mode");
   darkIcon.setAttribute("style","display:none;");
+  catSun.setAttribute("style","display:none;");
+  catCloud.setAttribute("style","display:none;");
 } else {
   lightIcon.setAttribute("style","display:none;");
+  catMoon.setAttribute("style","display:none;");
+  catStars.setAttribute("style","display:none;")
 }
 
 // Toggle dark mode on button click
@@ -23,10 +32,24 @@ function toggleDarkMode() {
   
     // Toggle light and dark icons
     if (darkMode) {
+      // In Dark mode, show sun icon and hide moon icon
       lightIcon.setAttribute("style","display:;");
       darkIcon.setAttribute("style","display:none;");
+
+      // In Dark mode, shows Moon and Stars in the Cat's window
+      catMoon.setAttribute("style","display:;");
+      catStars.setAttribute("style","display:;")
+      catSun.setAttribute("style","display:none;");
+      catCloud.setAttribute("style","display:none;");
     } else {
+      // In Light mode, show moon icon and hide sun icon
       lightIcon.setAttribute("style","display:none;");
       darkIcon.setAttribute("style","display:;");
+
+      // In Light mode, shows Sun and Cloud in the Cat's window
+      catSun.setAttribute("style","display:;");
+      catCloud.setAttribute("style","display:;");
+      catMoon.setAttribute("style","display:none;");
+      catStars.setAttribute("style","display:none;")
     }
   }
